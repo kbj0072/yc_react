b2b262c5748088b0bc698a1bafa56abcd7081362
Beomjun Kim <kbj0072@gmail.com>
2025-07-19 22:13:26 +0900

    p. 158, 라이브러리 사용하기

--- s03/chapter3/index.js
+++ s03/chapter3/index.js
@@ -1,3 +1,6 @@
-import circle from "./circle.js";
+import lodash from "lodash";
 
-console.log(circle.PI, circle.getArea(1), circle.getCircumference(1));
+const arr = [1, 1, 1, 2, 2, 1, 1, 4, 4, 3, 2];
+const uniqueArr = lodash.uniqBy(arr); // Using lodash to get unique values
+
+console.log(uniqueArr); // [1, 2, 4, 3]
