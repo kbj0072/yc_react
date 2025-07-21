fb06013a86a418ec425fe1c9868125d87ce38ee7
Beomjun Kim <kbj0072@gmail.com>
2025-07-22 05:31:31 +0900

    p.189, 컴포넌트를 페이지에 렌더링하기

--- s05/chapter5/src/App.js
+++ s05/chapter5/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
