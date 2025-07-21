3b43d794591ce2ec53341d0270f13c1b6c24349c
Beomjun Kim <kbj0072@gmail.com>
2025-07-22 05:40:56 +0900

    p.199, 사용할 수 없는 값

--- s05/chapter5/src/component/Body.js
+++ s05/chapter5/src/component/Body.js
@@ -1,10 +1,13 @@
 function Body() {
-  const boolA = true;
-  const boolB = false;
+  const objA = {
+    a: 1,
+    b: 2,
+  };
   return (
     <div>
       <h1>Body</h1>
-      <h2>{String(boolA || boolB)}</h2>
+      <h2>a: {objA.a}</h2>
+      <h2>b: {objA.b}</h2>
     </div>
   );
 }
