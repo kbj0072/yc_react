e7edf6643c20e4e5e312de87da5b80d170270f19
Beomjun Kim <kbj0072@gmail.com>
2025-07-22 05:42:01 +0900

    p.201, 닫힘 규칙

--- s05/chapter5/src/component/Body.js
+++ s05/chapter5/src/component/Body.js
@@ -1,13 +1,8 @@
 function Body() {
-  const objA = {
-    a: 1,
-    b: 2,
-  };
+
   return (
     <div>
-      <h1>Body</h1>
-      <h2>a: {objA.a}</h2>
-      <h2>b: {objA.b}</h2>
+      <h1>Body
     </div>
   );
 }
