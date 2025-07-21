195720a9b4338418910992376c2857cb07bd22dc
Beomjun Kim <kbj0072@gmail.com>
2025-07-22 05:35:21 +0900

    p.191, 컴포넌트별로 파일 분리하기

--- s05/chapter5/src/App.js
+++ s05/chapter5/src/App.js
@@ -1,17 +1,14 @@
 import "./App.css";
-
-function Header() {
-  return (
-    <header>
-      <h1>header</h1>
-    </header>
-  );
-}
+import Body from "./component/Body";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
